9a05670c9ecc91cbbdbbaac540d4288e3ed7d07f
emulhern-iqnt <edward.mulhern@sinch.com>
2025-09-09 15:13:17 -0600

    get agent able to use our function

--- streamlit-test.py
+++ streamlit-test.py
@@ -2,10 +2,18 @@ import streamlit as st
 import requests
 from llama_index.llms.ollama import Ollama
 
+from llama_index.core.tools import FunctionTool
+
+
 llm: Ollama = Ollama(base_url="http://44.200.48.59:11434", model="llama3.2")
 print(llm.complete("Tell me a joke"))
 
+from llama_index.llms.openai import OpenAI
+from llama_index.core.agent import FunctionAgent
+from llama_index.core.agent import ReActAgent
 
+
+llm2 = OpenAI(model="gpt-4o-mini")
 
 API_URL = "https://api.aurora-dev.sinchlab.com/AuroraService/v1/"
 USERNAME = "fb4b663c9ae241a58ac8239f910ca88c"
@@ -62,6 +70,19 @@ def api_response():
         return data
     else:
         return "Error contacting API."
+
+# Wrap our function as a Tool
+post_search_tool = FunctionTool.from_defaults(
+    fn=api_response,
+    name="PostSearchTool",
+    description="Get all orders from the API",
+)
+
+agent = ReActAgent(
+    tools=[post_search_tool],
+    llm=llm2,
+    verbose=True
+)
 
 def chatbot_response(message):
     return llm.complete(message)
