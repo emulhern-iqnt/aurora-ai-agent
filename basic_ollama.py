62b806b94b4c1e55b420268f24a5da5b7a2670cb
emulhern-iqnt <edward.mulhern@sinch.com>
2025-09-17 14:10:34 -0600

    cleanup

--- basic_ollama.py
+++
@@ -1,4 +0,0 @@
-from llama-index.llms.ollama import Ollama
-
-llm: Ollama = Ollama(base_url="http://44.200.48.59:11434", model="llama3.2")
-print(llm.complete("Tell me a joke"))
---
+++ test-mcp.py
@@ -0,0 +1,12 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP(name="CalculatorServer")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Adds two integer numbers together."""
+    return a + b
+
+    # server.py (continued)
+if __name__ == "__main__":
+   mcp.run()
