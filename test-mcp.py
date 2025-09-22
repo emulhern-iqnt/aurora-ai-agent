4336b66ef881ca575053921a5bd3d090499b9a86
emulhern-iqnt <edward.mulhern@sinch.com>
2025-09-22 13:34:27 -0600

    change names of mcp tools

--- test-mcp.py
+++ test-mcp.py
@@ -8,13 +8,13 @@ API_PASSWORD = "3a112f0cca8648378e4a2291f64a0b78"
 import requests
 
 @mcp.tool()
-def add(a: int, b: int) -> int:
+def put_together(a: int, b: int) -> int:
     print("Calling the 'add' function")
     """Adds two integer numbers together."""
     return a + b
 
 @mcp.tool()
-def adjust(a: int, b: int) -> int:
+def custom_manipulation(a: int, b: int) -> int:
     print("Calling the 'adjust' function")
     first = a + 1
     second = b + 1
